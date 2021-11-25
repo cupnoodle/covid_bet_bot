e4cb5c4fb45fffa9b2b30ab80a19ae1fec9d4370
Axel <axelshaded@gmail.com>
2021-11-25 17:45:11 +0800

    change timing

--- app.rb
+++ app.rb
@@ -126,8 +126,8 @@ post "/webhook" do
     elsif num.to_i > 1_000_000
       bot.send_message(chat_id: chat_id, text: "Please bet a smaller number, lul")
     else
-      if Time.now.getlocal('+08:00').hour >= 14 && Time.now.getlocal('+08:00').hour < 19
-        bot.send_message(chat_id: chat_id, text: "Sorry, you can't bet after 2pm, the timer will reset at 7pm")
+      if Time.now.getlocal('+08:00').hour >= 12 && Time.now.getlocal('+08:00').hour < 16
+        bot.send_message(chat_id: chat_id, text: "Sorry, you can't bet after 12pm, the timer will reset at 4pm")
         return '{}'
       else
         vote = Vote.where(poll_id: poll.id, user_id: user.id).first_or_create.update(answer: num)
